4eb1b56655a7bcafb61a75cfbcc5f0ad58c74565
nurislam <mridha851@gmail.com>
2021-05-24 19:45:07 +0600

    Updated:

--- src/Components/health/_redux/action/HealthAction.js
+++ src/Components/health/_redux/action/HealthAction.js
@@ -137,4 +137,3 @@ export const ResendHealthOtp = (mobile) => async (dispatch) => {
 export const EmptyHealthOtp = () => (dispatch) => {
   dispatch({ type: Types.HEALTH_OTP_ID, payload: null });
 };
-
